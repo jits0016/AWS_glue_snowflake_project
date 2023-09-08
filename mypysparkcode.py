157f704fdb0e2419358e7add690b424623689f9f
jits0016 <jitsdataengineer16@gmail.com>
2023-09-08 10:05:04 +0530

    add requirment.txt and update and add lines py file

--- mypysparkcode.py
+++ mypysparkcode.py
@@ -11,6 +11,13 @@ res=df.withColumn('theme1name',col('theme1.Name')).withColumn('theme1percent',co
     .withColumn('theme_namecode',explode(col('theme_namecode'))).\
     withColumn('theme_code',col('theme_namecode.code'))\
     .withColumn('theme_name',col('theme_namecode.name')).drop('theme_namecode','theme1')
+
+
+
+
+
+
+
 final=res.select('theme1name','theme1percent','theme_name')
 output_path=r"F:\aws_project_1"
 final.write.mode('overwrite').format('csv').option('header','false').save(output_path)
